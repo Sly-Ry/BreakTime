f0097d113000cb766a783fdf8cebd8e6f46d9d9b
Ryan Williams <ryan.williams93@aol.com>
2023-06-09 16:30:58 -0500

    made initial styles, but incorporating Materialize css framework

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+export default function App() {
+
+  const [timer, setTimer] = useState(25 * 60);
+
+  const formatTime = (time) => {
+    let minutes = Math.floor(time/60);
+    let seconds = time % 60;
+
+    return (
+      (minutes < 10 ? "0" + minutes : minutes) + 
+      ":" +
+      (seconds < 10 ? "0" + seconds : seconds) 
+    );
+  };
+
   return (
     <div className="App">
-      
+      <h1>BreakTime</h1>
+      <Label title={"Break Length"}></Label>
+      <Label title={"Session Length"}></Label>
+      <br/>
+      <div id='timer'>
+        <h2 id='timer-label'>Session</h2>
+        <p id='time-left'>{formatTime(timer)}</p>
+      </div>
+      <div id='timer-control'>
+        <button id='start_stop'>Play/pause</button>
+        <button id='reset'>reset</button>
+      </div>
     </div>
   );
 }
 
-export default App;
+function Label({ title, changeTime, type, time, formatTime }) {
+  return (
+    <div id='break-label'>
+        <h3 id='break-length'>{title}</h3>
+        <div className='buttons'>
+          <button id='break-dec'>-</button>
+          <h3>5</h3>
+          <button id='break-inc'>+</button>
+        </div>
+      </div>
+  )
+}
